deb7ca9971673eeaa0446079813b21ea1d239a04
まの <48278137+Manoruchan@users.noreply.github.com>
2024-09-08 17:14:07 +0900

    Update sdvx-keyboard-controller.ino

--- sdvx-keyboard-controller.ino
+++ sdvx-keyboard-controller.ino
@@ -48,171 +48,175 @@ volatile int8_t encStateR = 0;
 
 
 void setup() {
-	// pin settings
+    // pin settings
 
-	// rorary encoders
-	pinMode(PIN_VOL_L_A, INPUT_PULLUP);
-	pinMode(PIN_VOL_L_B, INPUT_PULLUP);
-	pinMode(PIN_VOL_R_A, INPUT_PULLUP);
-	pinMode(PIN_VOL_R_B, INPUT_PULLUP);
+    // rorary encoders
+    pinMode(PIN_VOL_L_A, INPUT_PULLUP);
+    pinMode(PIN_VOL_L_B, INPUT_PULLUP);
+    pinMode(PIN_VOL_R_A, INPUT_PULLUP);
+    pinMode(PIN_VOL_R_B, INPUT_PULLUP);
 
-	// buttons
-	pinMode(PIN_BT_A, INPUT_PULLUP);
-	pinMode(PIN_BT_B, INPUT_PULLUP);
-	pinMode(PIN_BT_C, INPUT_PULLUP);
-	pinMode(PIN_BT_D, INPUT_PULLUP);
-	pinMode(PIN_FX_L, INPUT_PULLUP);
-	pinMode(PIN_FX_R, INPUT_PULLUP);
-	pinMode(PIN_BT_START, INPUT_PULLUP);
+    // buttons
+    pinMode(PIN_BT_A, INPUT_PULLUP);
+    pinMode(PIN_BT_B, INPUT_PULLUP);
+    pinMode(PIN_BT_C, INPUT_PULLUP);
+    pinMode(PIN_BT_D, INPUT_PULLUP);
+    pinMode(PIN_FX_L, INPUT_PULLUP);
+    pinMode(PIN_FX_R, INPUT_PULLUP);
+    pinMode(PIN_BT_START, INPUT_PULLUP);
 
-	// enc interrupts
-	attachInterrupt(digitalPinToInterrupt(PIN_VOL_L_A), updateEncStateL, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(PIN_VOL_L_B), updateEncStateL, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(PIN_VOL_R_A), updateEncStateR, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(PIN_VOL_R_B), updateEncStateR, CHANGE);
+    // enc interrupts
+    attachInterrupt(digitalPinToInterrupt(PIN_VOL_L_A), updateEncStateL,
+        CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_VOL_L_B), updateEncStateL,
+        CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_VOL_R_A), updateEncStateR,
+        CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_VOL_R_B), updateEncStateR,
+        CHANGE);
 
-	// use as keyboard
-	Keyboard.begin();
+    // use as keyboard
+    Keyboard.begin();
 }
 
 
 void loop() {
-	// button
-	if (digitalRead(PIN_BT_A) == LOW) {
-		Keyboard.press(BT_A);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_BT_A) == HIGH) {
-		Keyboard.release(BT_A);
-	}
+    // button
+    if (digitalRead(PIN_BT_A) == LOW) {
+        Keyboard.press(BT_A);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_BT_A) == HIGH) {
+        Keyboard.release(BT_A);
+    }
 
-	if (digitalRead(PIN_BT_B) == LOW) {
-		Keyboard.press(BT_B);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_BT_B) == HIGH) {
-		Keyboard.release(BT_B);
-	}
+    if (digitalRead(PIN_BT_B) == LOW) {
+        Keyboard.press(BT_B);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_BT_B) == HIGH) {
+        Keyboard.release(BT_B);
+    }
 
-	if (digitalRead(PIN_BT_C) == LOW) {
-		Keyboard.press(BT_C);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_BT_C) == HIGH) {
-		Keyboard.release(BT_C);
-	}
+    if (digitalRead(PIN_BT_C) == LOW) {
+        Keyboard.press(BT_C);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_BT_C) == HIGH) {
+        Keyboard.release(BT_C);
+    }
 
-	if (digitalRead(PIN_BT_D) == LOW) {
-		Keyboard.press(BT_D);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_BT_D) == HIGH) {
-		Keyboard.release(BT_D);
-	}
+    if (digitalRead(PIN_BT_D) == LOW) {
+        Keyboard.press(BT_D);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_BT_D) == HIGH) {
+        Keyboard.release(BT_D);
+    }
 
-	if (digitalRead(PIN_FX_L) == LOW) {
-		Keyboard.press(FX_L);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_FX_L) == HIGH) {
-		Keyboard.release(FX_L);
-	}
+    if (digitalRead(PIN_FX_L) == LOW) {
+        Keyboard.press(FX_L);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_FX_L) == HIGH) {
+        Keyboard.release(FX_L);
+    }
 
-	if (digitalRead(PIN_FX_R) == LOW) {
-		Keyboard.press(FX_R);
-		delay(KEY_DELAY);
-	} else if (digitalRead(PIN_FX_R) == HIGH) {
-		Keyboard.release(FX_R);
-	}
+    if (digitalRead(PIN_FX_R) == LOW) {
+        Keyboard.press(FX_R);
+        delay(KEY_DELAY);
+    } else if (digitalRead(PIN_FX_R) == HIGH) {
+        Keyboard.release(FX_R);
+    }
 
-	if (digitalRead(PIN_BT_START) == LOW) {
-		Keyboard.press(KEY_RETURN);
-	} else if (digitalRead(PIN_BT_START) == HIGH) {
-		Keyboard.release(KEY_RETURN);
-	}
+    if (digitalRead(PIN_BT_START) == LOW) {
+        Keyboard.press(KEY_RETURN);
+    } else if (digitalRead(PIN_BT_START) == HIGH) {
+        Keyboard.release(KEY_RETURN);
+    }
 
-	// convert enc input to keyboard input
-	// clockwise
-	if (encStateL > 0) {
-		Keyboard.release(VOL_L_CCW);
-		Keyboard.press(VOL_L_CW);
-		delay(VOL_RELEASE_DELAY);
-		Keyboard.release(VOL_L_CW);
-		encStateL--;
-	}
-	// counterclockwise
-	if (encStateL < 0) {
-		Keyboard.release(VOL_L_CW);
-		Keyboard.press(VOL_L_CCW);
-		delay(VOL_RELEASE_DELAY);
-		Keyboard.release(VOL_L_CCW);
-		encStateL++;
-	}
+    // convert enc input to keyboard input
+    // clockwise
+    if (encStateL > 0) {
+        Keyboard.release(VOL_L_CCW);
+        Keyboard.press(VOL_L_CW);
+        delay(VOL_RELEASE_DELAY);
+        Keyboard.release(VOL_L_CW);
+        encStateL--;
+    }
+    // counterclockwise
+    if (encStateL < 0) {
+        Keyboard.release(VOL_L_CW);
+        Keyboard.press(VOL_L_CCW);
+        delay(VOL_RELEASE_DELAY);
+        Keyboard.release(VOL_L_CCW);
+        encStateL++;
+    }
 
-	// clockwise
-	if (encStateR > 0) {
-		Keyboard.release(VOL_R_CCW);
-		Keyboard.press(VOL_R_CW);
-		delay(VOL_RELEASE_DELAY);
-		Keyboard.release(VOL_R_CW);
-		encStateR--;
-	}
-	// counterclockwise
-	if (encStateR < 0) {
-		Keyboard.release(VOL_R_CW);
-		Keyboard.press(VOL_R_CCW);
-		delay(VOL_RELEASE_DELAY);
-		Keyboard.release(VOL_R_CCW);
-		encStateR++;
-	}
+    // clockwise
+    if (encStateR > 0) {
+        Keyboard.release(VOL_R_CCW);
+        Keyboard.press(VOL_R_CW);
+        delay(VOL_RELEASE_DELAY);
+        Keyboard.release(VOL_R_CW);
+        encStateR--;
+    }
+    // counterclockwise
+    if (encStateR < 0) {
+        Keyboard.release(VOL_R_CW);
+        Keyboard.press(VOL_R_CCW);
+        delay(VOL_RELEASE_DELAY);
+        Keyboard.release(VOL_R_CCW);
+        encStateR++;
+    }
 }
 
 
 void updateEncStateL() {
-	uint8_t pinA = digitalRead(PIN_VOL_L_A);
-	uint8_t pinB = digitalRead(PIN_VOL_L_B);
+    uint8_t pinA = digitalRead(PIN_VOL_L_A);
+    uint8_t pinB = digitalRead(PIN_VOL_L_B);
 
-	encCurrentL = (pinA << 1) | pinB;
+    encCurrentL = (pinA << 1) | pinB;
 
-	if (!(encCurrentL == encPreviousL)) {
-		uint8_t encState = encPastL << 4 | encPreviousL << 2 | encCurrentL;
+    if (!(encCurrentL == encPreviousL)) {
+        uint8_t encState = encPastL << 4 | encPreviousL << 2 | encCurrentL;
 
-		// if clockwise
-		if (encState == 0b111000) {
-			if (encStateL != 1) {
-				encStateL++;
-			}
-		}
-		// if conterclockwise
-		else if (encState == 0b001011) {
-			if (encStateL != -1) {
-				encStateL--;
-			}
-		}
+        // if clockwise
+        if (encState == 0b111000) {
+            if (encStateL != 1) {
+                encStateL++;
+            }
+        }
+        // if conterclockwise
+        else if (encState == 0b001011) {
+            if (encStateL != -1) {
+                encStateL--;
+            }
+        }
 
-		encPastL = encPreviousL;
-		encPreviousL = encCurrentL;
-	}
+        encPastL = encPreviousL;
+        encPreviousL = encCurrentL;
+    }
 }
 
 void updateEncStateR() {
-	uint8_t pinA = digitalRead(PIN_VOL_R_A);
-	uint8_t pinB = digitalRead(PIN_VOL_R_B);
+    uint8_t pinA = digitalRead(PIN_VOL_R_A);
+    uint8_t pinB = digitalRead(PIN_VOL_R_B);
 
-	encCurrentR = (pinA << 1) | pinB;
+    encCurrentR = (pinA << 1) | pinB;
 
-	if (!(encCurrentR == encPreviousR)) {
-		uint8_t encState = encPastR << 4 | encPreviousR << 2 | encCurrentR;
+    if (!(encCurrentR == encPreviousR)) {
+        uint8_t encState = encPastR << 4 | encPreviousR << 2 | encCurrentR;
 
-		// if clockwise
-		if (encState == 0b111000) {
-			if (encStateR != 1) {
-				encStateR++;
-			}
-		}
-		// if conterclockwise
-		else if (encState == 0b001011) {
-			if (encStateR != -1) {
-				encStateR--;
-			}
-		}
+        // if clockwise
+        if (encState == 0b111000) {
+            if (encStateR != 1) {
+                encStateR++;
+            }
+        }
+        // if conterclockwise
+        else if (encState == 0b001011) {
+            if (encStateR != -1) {
+                encStateR--;
+            }
+        }
 
-		encPastR = encPreviousR;
-		encPreviousR = encCurrentR;
-	}
+        encPastR = encPreviousR;
+        encPreviousR = encCurrentR;
+    }
 }
